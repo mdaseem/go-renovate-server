cd7e26266b0a6b1ba03c7a5c17549117fc4e5b01
Mohammed Aseem <mohammed.aseem@publicissapient.com>
2026-05-02 17:34:53 +0530

    Socket setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv"; 
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import http from "http";
 import cors from "cors";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
@@ -7,13 +8,19 @@ import prductRoutes from "./routes/prductRoutes"
 import addUser from "./routes/addUser"
 import authorize from "./routes/authorizeUser"
 import { requireAuth } from "./middleware/authMiddleware";
+import { Server } from "socket.io";
 
 
+interface MessageData {
+  roomId: string;
+  message: string;
+  sender: string;
+}
 
 dotenv.config();
 mongoose.connect("mongodb+srv://go-renovate-userDB:3AsOY7MQsPeCNaYV@cluster0.cjpxkja.mongodb.net/test")
-  .then(() => console.log("✅ Connected to MongoDB Atlas"))
-  .catch(err => console.error("❌ Connection error:", err));
+  .then(() => console.log(" Connected to MongoDB Atlas"))
+  .catch(err => console.error(" Connection error:", err));
 
 mongoose.connection.on("open", () => {
   console.log(`Online Training DB connected !`);
@@ -22,14 +29,46 @@ mongoose.connection.on("open", () => {
 const app: Express = express();
 const port = process.env.SERVER_PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+// app.use(cors());
 app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/signup", addUser)
 app.use("/auth", authorize);
 app.use("/products",requireAuth, prductRoutes); 
 
+const server = http.createServer(app);
 
-app.listen(port, () => {
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+// 👇 socket logic
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (roomId: string) => {
+    socket.join(roomId);
+  });
+
+  socket.on("send_message", (data: MessageData) => {
+    socket.to(data.roomId).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(port, () => {
     console.log(`Server running @ port ${port} !`);
   });
